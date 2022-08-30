33adab082665fc3b88a8feb29376bbf2929c9d75
Flarmoni <mustiozgun@gmail.com>
2022-08-30 15:40:21 +0300

    cors updated

--- routers/index.js
+++ routers/index.js
@@ -16,14 +16,14 @@ const corsOptions = {
 };
 router.use(cors(corsOptions));
 
-router.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
+// router.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept',
+//   );
+//   next();
+// });
 router.use(express.static('public'));
 
 router.use('/categories', categories);
